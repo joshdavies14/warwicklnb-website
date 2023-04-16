76433c0fe65384117e460b256454f31010143ba0
Josh Davies <josh@joshdavies.tech>
2023-04-16 20:19:41 +0100

    :art: Prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,7 @@
     <main>
         <div class="head-block-home">
             <section
-                class="
-                    head-inner
-                    align-middle
-                    h-100
-                    d-flex
-                    align-items-center
-                    justify-content-center
-                "
+                class="head-inner align-middle h-100 d-flex align-items-center justify-content-center"
             >
                 <h1 class="display-2 fw-bold">warwick latin & ballroom</h1>
             </section>
@@ -22,12 +15,7 @@
                 hello! 👋
             </h2>
             <p
-                class="
-                    d-flex
-                    align-items-center
-                    justify-content-center
-                    text-center
-                "
+                class="d-flex align-items-center justify-content-center text-center"
             >
                 Warwick University Dancesport is dedicated to providing
                 students, of all abilities, the opportunities to dance Latin and
